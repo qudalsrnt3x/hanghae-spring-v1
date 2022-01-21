4ce3c986b9ecdaf1cd3de92384c930a6809b41fe
qudalsrnt3x <qudalsrnt3x@gmail.com>
2022-01-21 16:46:04 +0900

    Init Commit

---
+++ src/main/java/com/hanghae/hanghae_spring_prac/HanghaeSpringPracApplication.java
@@ -0,0 +1,13 @@
+package com.hanghae.hanghae_spring_prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaeSpringPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanghaeSpringPracApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanghae/hanghae_spring_prac/HanghaeSpringPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae.hanghae_spring_prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanghaeSpringPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
