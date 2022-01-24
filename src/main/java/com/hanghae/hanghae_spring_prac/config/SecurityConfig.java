728e606c117584f41e942eee07676d881fd6294a
qudalsrnt3x <qudalsrnt3x@gmail.com>
2022-01-24 22:03:48 +0900

    Add 로그아웃 기능 구현

--- src/main/java/com/hanghae/hanghae_spring_prac/config/SecurityConfig.java
+++ src/main/java/com/hanghae/hanghae_spring_prac/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
 
                 .logout()
-                .logoutSuccessUrl("/login")
+                .logoutSuccessUrl("/loginForm")
                 .invalidateHttpSession(true);
 
     }
